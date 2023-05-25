d2923706d4575906beae46c64218a3333043c30b
Joppe Koers <joppe@joppekoers.nl>
2023-05-25 18:52:22 +0200

    Add more test cases, better failed test printing

--- test/src/main.c
+++ test/src/main.c
@@ -14,32 +14,38 @@ void write_test_data(void* ptr, size_t size)
 	}
 }
 
-bool is_valid_test_data(void* ptr, size_t size)
+void assert_test_data(void* ptr, size_t size)
 {
 	size_t	i = 0;
 	uint8_t c = 0;
 
 	while (i < size)
 	{
-		if (((char*)ptr)[i] != c++)
-			return false;
+		if (((uint8_t*)ptr)[i] != c)
+		{
+			printf("Test data does not match at index %lu\n", i);
+			printf("Expected: %d\n", c);
+			printf("Got     : %d\n", ((uint8_t*)ptr)[i]);
+			exit(1);
+		}
+		c++;
 		i++;
 	}
-	return true;
 }
 
-void assert_test_data(void* ptr, size_t size)
+void test_malloc(size_t size)
 {
-	if (!is_valid_test_data(ptr, size))
-		EXIT_WITH_ERROR("Test data is not valid");
+	void* ptr = malloc(size);
+	write_test_data(ptr, size);
+	assert_test_data(ptr, size);
+	// ft_free(ptr);
 }
 
 void run_tests()
 {
-	void* ptr = ft_malloc(100);
-	write_test_data(ptr, 100);
-	assert_test_data(ptr, 100);
-	// ft_free(ptr);
+	test_malloc(1);
+	test_malloc(100);
+	test_malloc(10000);
 }
 
 int main()
@@ -50,4 +56,5 @@ int main()
 	}
 
 	printf("Test passed\n");
+	return 0;
 }
