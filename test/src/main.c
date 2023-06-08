6e2cd69211a0315d0feec9825ab72c973b24ab05
Joppe Koers <joppe@joppekoers.nl>
2023-06-09 00:44:48 +0200

    Make test more realistic

--- test/src/main.c
+++ test/src/main.c
@@ -3,9 +3,13 @@
 #include <stdio.h>
 #include <time.h>
 #define BILLION 1000000000L
-size_t allocations = 0;
+#define MAX_ALLOCATIONS 10000000
+size_t	  allocations_count = 0;
+size_t	  allocations_i = 0;
+void*	  allocations[MAX_ALLOCATIONS];
+long long allocated_bytes = 0;
 
-void   write_test_data(void* ptr, size_t size)
+void	  write_test_data(void* ptr, size_t size)
 {
 	size_t	i = 0;
 	uint8_t c = 0;
@@ -36,20 +40,30 @@ void assert_test_data(void* ptr, size_t size)
 	}
 }
 
-void test_malloc(size_t size)
+void allocate_and_read_write(size_t size)
 {
 	void* ptr = ft_malloc(size);
-	allocations++;
 	write_test_data(ptr, size);
 	assert_test_data(ptr, size);
-	ft_free(ptr);
+	allocations[allocations_i++] = ptr;
+	allocations_count++;
+	allocated_bytes += size;
 }
 
 void run_tests()
 {
-	test_malloc(1);
-	test_malloc(100);
-	test_malloc(10000);
+	const size_t iterations = 10;
+	allocations_i = 0;
+	for (size_t i = 0; i < iterations; i++)
+	{
+		allocate_and_read_write(1);
+		allocate_and_read_write(100);
+		allocate_and_read_write(1000);
+	}
+	for (size_t i = 0; i < allocations_i; i++)
+	{
+		ft_free(allocations[i]);
+	}
 }
 
 struct timespec start_clock()
@@ -80,9 +94,10 @@ int main()
 	long long duration = get_duration_ns(time);
 	print_allocations();
 	printf("Test passed\n");
-	printf("  Allocations       : %lu\n", allocations);
-	const float allocs_per_sec = (float)allocations / (float)duration * BILLION;
+	printf("  Allocations       : %.1e - %lu \n", (float)allocations_count, allocations_count);
+	const float allocs_per_sec = (float)allocations_count / (float)duration * BILLION;
 	printf("  Allocations/second: %.1e - %f\n", allocs_per_sec, allocs_per_sec);
+	printf("  Allocated         : %.2f MB - %lld bytes\n", allocated_bytes / 1000000.0, allocated_bytes);
 	printf("  Duration          : %lld ms - %lld ns\n", duration / 1000000, duration);
 
 	return 0;
